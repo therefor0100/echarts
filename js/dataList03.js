77670ee5ef69d81f1fa63e9c6ec406b64bd05f7e
uName <1044078326@qq.com>
2018-10-30 15:48:33 +0800

    first commmit

---
+++ js/dataList03.js
@@ -0,0 +1,1198 @@
+var dataList03=[
+    {
+        "number": "060****001",
+        "from": "GWC1SS",
+        "time": "55"
+    },
+
+    {
+        "number": "072****560",
+        "from": "GWB2",
+        "time": "9"
+    },
+
+    {
+        "number": "153****515",
+        "from": "LTSSB4",
+        "time": "16"
+    },
+
+    {
+        "number": "133****296",
+        "from": "LTSSB3",
+        "time": "17"
+    },
+
+    {
+        "number": "072****623",
+        "from": "TGW1",
+        "time": "43"
+    },
+
+    {
+        "number": "060****418",
+        "from": "GWC2SS",
+        "time": "29"
+    },
+
+    {
+        "number": "173****904",
+        "from": "LTSSB3",
+        "time": "11"
+    },
+
+    {
+        "number": "060****765",
+        "from": "民生SS1",
+        "time": "144"
+    },
+
+    {
+        "number": "189****089",
+        "from": "LTSSB4",
+        "time": "66"
+    },
+
+    {
+        "number": "072****045",
+        "from": "GWB1",
+        "time": "8"
+    },
+
+    {
+        "number": "060****764",
+        "from": "GWC1SS",
+        "time": "30"
+    },
+
+    {
+        "number": "060****822",
+        "from": "GWC1SS",
+        "time": "0"
+    },
+
+    {
+        "number": "181****277",
+        "from": "LTSSB4",
+        "time": "15"
+    },
+
+    {
+        "number": "060****768",
+        "from": "民生SS1",
+        "time": "0"
+    },
+
+    {
+        "number": "072****206",
+        "from": "GWB1",
+        "time": "9"
+    },
+
+    {
+        "number": "072****188",
+        "from": "GWB1",
+        "time": "7"
+    },
+
+    {
+        "number": "072****578",
+        "from": "XWTGW2",
+        "time": "0"
+    },
+
+    {
+        "number": "008****030",
+        "from": "XWTGW1",
+        "time": "254"
+    },
+
+    {
+        "number": "060****124",
+        "from": "GWC2SS",
+        "time": "54"
+    },
+
+    {
+        "number": "072****306",
+        "from": "XWTGW2",
+        "time": "12"
+    },
+
+    {
+        "number": "133****177",
+        "from": "LTSSB3",
+        "time": "1"
+    },
+
+    {
+        "number": "133****017",
+        "from": "LTSSB4",
+        "time": "81"
+    },
+
+    {
+        "number": "060****256",
+        "from": "GWC1SS",
+        "time": "0"
+    },
+
+    {
+        "number": "133****089",
+        "from": "LTSSB4",
+        "time": "12"
+    },
+
+    {
+        "number": "060****128",
+        "from": "GWC1SS",
+        "time": "15"
+    },
+
+    {
+        "number": "189****192",
+        "from": "LTSSB3",
+        "time": "17"
+    },
+
+    {
+        "number": "153****011",
+        "from": "LTSSB4",
+        "time": "11"
+    },
+
+    {
+        "number": "060****3203",
+        "from": "GWC1SS",
+        "time": "71"
+    },
+
+    {
+        "number": "173****465",
+        "from": "LTSSB3",
+        "time": "7"
+    },
+
+    {
+        "number": "063****578",
+        "from": "GWC1SS",
+        "time": "43"
+    },
+
+    {
+        "number": "133****736",
+        "from": "LTSSB3",
+        "time": "15"
+    },
+
+    {
+        "number": "133****836",
+        "from": "LTSSB4",
+        "time": "85"
+    },
+
+    {
+        "number": "038****808",
+        "from": "GWB1",
+        "time": "0"
+    },
+
+    {
+        "number": "180****856",
+        "from": "LTSSB4",
+        "time": "12"
+    },
+
+    {
+        "number": "072****525",
+        "from": "GWB1",
+        "time": "7"
+    },
+
+    {
+        "number": "072****849",
+        "from": "GWB2",
+        "time": "10"
+    },
+
+    {
+        "number": "189****052",
+        "from": "LTSSB3",
+        "time": "44"
+    },
+
+    {
+        "number": "008****780",
+        "from": "XWTGW2",
+        "time": "39"
+    },
+
+    {
+        "number": "133****300",
+        "from": "LTSSB4",
+        "time": "772"
+    },
+
+    {
+        "number": "177****346",
+        "from": "LTSSB3",
+        "time": "11"
+    },
+
+    {
+        "number": "173****241",
+        "from": "LTSSB4",
+        "time": "11"
+    },
+
+    {
+        "number": "072****065",
+        "from": "GWA1",
+        "time": "9"
+    },
+
+    {
+        "number": "060****760",
+        "from": "民生SS1",
+        "time": "137"
+    },
+
+    {
+        "number": "000****000",
+        "from": "GWC2SS",
+        "time": "57"
+    },
+
+    {
+        "number": "189****324",
+        "from": "LTSSB3",
+        "time": "9"
+    },
+
+    {
+        "number": "060****981",
+        "from": "GWC1SS",
+        "time": "41"
+    },
+
+    {
+        "number": "133****300",
+        "from": "LTSSB3",
+        "time": "40"
+    },
+
+    {
+        "number": "072****688",
+        "from": "8SS2",
+        "time": "10"
+    },
+
+    {
+        "number": "189****015",
+        "from": "LTSSB4",
+        "time": "34"
+    },
+
+    {
+        "number": "189****320",
+        "from": "LTSSB4",
+        "time": "45"
+    },
+
+    {
+        "number": "060****160",
+        "from": "2SS1",
+        "time": "73"
+    },
+
+    {
+        "number": "189****244",
+        "from": "LTSSB4",
+        "time": "20"
+    },
+
+    {
+        "number": "173****456",
+        "from": "LTSSB3",
+        "time": "9"
+    },
+
+    {
+        "number": "133****677",
+        "from": "LTSSB3",
+        "time": "8"
+    },
+
+    {
+        "number": "060****352",
+        "from": "GWC2SS",
+        "time": "87"
+    },
+
+    {
+        "number": "060****541",
+        "from": "GWC1SS",
+        "time": "48"
+    },
+
+    {
+        "number": "046****353",
+        "from": "XWTGW1",
+        "time": "0"
+    },
+
+    {
+        "number": "133****085",
+        "from": "LTSSB3",
+        "time": "35"
+    },
+
+    {
+        "number": "180****800",
+        "from": "LTSSB3",
+        "time": "10"
+    },
+
+    {
+        "number": "060****531",
+        "from": "GWC1SS",
+        "time": "13"
+    },
+
+    {
+        "number": "133****679",
+        "from": "LTSSB4",
+        "time": "65"
+    },
+
+    {
+        "number": "008****0719",
+        "from": "GWB1",
+        "time": "0"
+    },
+
+    {
+        "number": "060****130",
+        "from": "GWC2SS",
+        "time": "0"
+    },
+
+    {
+        "number": "072****901",
+        "from": "GWB1",
+        "time": "8"
+    },
+
+    {
+        "number": "133****370",
+        "from": "LTSSB3",
+        "time": "10"
+    },
+
+    {
+        "number": "046****238",
+        "from": "GWA2",
+        "time": "0"
+    },
+
+    {
+        "number": "180****065",
+        "from": "LTSSB3",
+        "time": "7"
+    },
+
+    {
+        "number": "133****597",
+        "from": "LTSSB4",
+        "time": "10"
+    },
+
+    {
+        "number": "177****239",
+        "from": "LTSSB3",
+        "time": "11"
+    },
+
+    {
+        "number": "189****319",
+        "from": "LTSSB3",
+        "time": "47"
+    },
+
+    {
+        "number": "072****660",
+        "from": "GWA2",
+        "time": "9"
+    },
+
+    {
+        "number": "173****147",
+        "from": "LTSSB4",
+        "time": "18"
+    },
+
+    {
+        "number": "153****527",
+        "from": "LTSSB4",
+        "time": "22"
+    },
+
+    {
+        "number": "189****082",
+        "from": "LTSSB3",
+        "time": "29"
+    },
+
+    {
+        "number": "061****349",
+        "from": "GWC1SS",
+        "time": "6"
+    },
+
+    {
+        "number": "153****9750",
+        "from": "LTSSB3",
+        "time": "11"
+    },
+
+    {
+        "number": "095****666",
+        "from": "4SS2",
+        "time": "34"
+    },
+
+    {
+        "number": "177****000",
+        "from": "LTSSB4",
+        "time": "38"
+    },
+
+    {
+        "number": "060****763",
+        "from": "民生SS1",
+        "time": "72"
+    },
+
+    {
+        "number": "072****118",
+        "from": "GWB2",
+        "time": "10"
+    },
+
+    {
+        "number": "173****553",
+        "from": "LTSSB4",
+        "time": "14"
+    },
+
+    {
+        "number": "133****391",
+        "from": "LTSSB3",
+        "time": "16"
+    },
+
+    {
+        "number": "072****220",
+        "from": "GWA2",
+        "time": "3"
+    },
+
+    {
+        "number": "189****036",
+        "from": "LTSSB3",
+        "time": "44"
+    },
+
+    {
+        "number": "072****098",
+        "from": "TGW2",
+        "time": "0"
+    },
+
+    {
+        "number": "060****520",
+        "from": "GWC1SS",
+        "time": "0"
+    },
+
+    {
+        "number": "180****760",
+        "from": "LTSSB4",
+        "time": "17"
+    },
+
+    {
+        "number": "173****973",
+        "from": "LTSSB4",
+        "time": "34"
+    },
+
+    {
+        "number": "173****184",
+        "from": "LTSSB4",
+        "time": "28"
+    },
+
+    {
+        "number": "133****975",
+        "from": "LTSSB3",
+        "time": "33"
+    },
+
+    {
+        "number": "173****615",
+        "from": "LTSSB3",
+        "time": "17"
+    },
+
+    {
+        "number": "153****310",
+        "from": "LTSSB3",
+        "time": "9"
+    },
+
+    {
+        "number": "177****792",
+        "from": "LTSSB3",
+        "time": "41"
+    },
+
+    {
+        "number": "133****735",
+        "from": "LTSSB4",
+        "time": "301"
+    },
+
+    {
+        "number": "072****933",
+        "from": "GWB2",
+        "time": "7"
+    },
+
+    {
+        "number": "060****823",
+        "from": "GWC2SS",
+        "time": "56"
+    },
+
+    {
+        "number": "180****996",
+        "from": "LTSSB4",
+        "time": "36"
+    },
+
+    {
+        "number": "181****211",
+        "from": "LTSSB3",
+        "time": "25"
+    },
+
+    {
+        "number": "133****877",
+        "from": "LTSSB3",
+        "time": "10"
+    },
+
+    {
+        "number": "072****159",
+        "from": "民生SS1",
+        "time": "10"
+    },
+
+    {
+        "number": "189****640",
+        "from": "LTSSB3",
+        "time": "29"
+    },
+
+    {
+        "number": "000",
+        "from": "TMS1",
+        "time": "18"
+    },
+
+    {
+        "number": "180****540",
+        "from": "LTSSB4",
+        "time": "19"
+    },
+
+    {
+        "number": "072****659",
+        "from": "GWB1",
+        "time": "8"
+    },
+
+    {
+        "number": "095****799",
+        "from": "TGW2",
+        "time": "62"
+    },
+
+    {
+        "number": "008****206",
+        "from": "XWTGW2",
+        "time": "0"
+    },
+
+    {
+        "number": "082****777",
+        "from": "GWB1",
+        "time": "14"
+    },
+
+    {
+        "number": "046****586",
+        "from": "3SS1",
+        "time": "7"
+    },
+
+    {
+        "number": "133****389",
+        "from": "LTSSB4",
+        "time": "32"
+    },
+
+    {
+        "number": "060****073",
+        "from": "2SS1",
+        "time": "13"
+    },
+
+    {
+        "number": "173****305",
+        "from": "LTSSB4",
+        "time": "5"
+    },
+
+    {
+        "number": "177****685",
+        "from": "LTSSB4",
+        "time": "30"
+    },
+
+    {
+        "number": "060****420",
+        "from": "2SS1",
+        "time": "21"
+    },
+
+    {
+        "number": "189****354",
+        "from": "LTSSB3",
+        "time": "246"
+    },
+
+    {
+        "number": "133****685",
+        "from": "LTSSB3",
+        "time": "3"
+    },
+
+    {
+        "number": "189****686",
+        "from": "LTSSB4",
+        "time": "36"
+    },
+
+    {
+        "number": "009****905",
+        "from": "国际SS",
+        "time": "0"
+    },
+
+    {
+        "number": "045****755",
+        "from": "XWTGW2",
+        "time": "17"
+    },
+
+    {
+        "number": "060****731",
+        "from": "GWC2SS",
+        "time": "76"
+    },
+
+    {
+        "number": "181****372",
+        "from": "LTSSB4",
+        "time": "14"
+    },
+
+    {
+        "number": "072****675",
+        "from": "GWA2",
+        "time": "11"
+    },
+
+    {
+        "number": "153****006",
+        "from": "LTSSB4",
+        "time": "52"
+    },
+
+    {
+        "number": "046****336",
+        "from": "XWTGW2",
+        "time": "8"
+    },
+
+    {
+        "number": "072****622",
+        "from": "3SS1",
+        "time": "11"
+    },
+
+    {
+        "number": "189****696",
+        "from": "LTSSB4",
+        "time": "218"
+    },
+
+    {
+        "number": "177****967",
+        "from": "LTSSB3",
+        "time": "15"
+    },
+
+    {
+        "number": "060****511",
+        "from": "2SS1",
+        "time": "54"
+    },
+
+    {
+        "number": "095****333",
+        "from": "XWTGW2",
+        "time": "154"
+    },
+
+    {
+        "number": "153****284",
+        "from": "LTSSB4",
+        "time": "59"
+    },
+
+    {
+        "number": "072****661",
+        "from": "GWA2",
+        "time": "9"
+    },
+
+    {
+        "number": "173****304",
+        "from": "LTSSB4",
+        "time": "12"
+    },
+
+    {
+        "number": "095****497",
+        "from": "YDSSB1",
+        "time": "0"
+    },
+
+    {
+        "number": "133****223",
+        "from": "LTSSB4",
+        "time": "37"
+    },
+
+    {
+        "number": "095****010",
+        "from": "4SS2",
+        "time": "59"
+    },
+
+    {
+        "number": "153****086",
+        "from": "LTSSB3",
+        "time": "26"
+    },
+
+    {
+        "number": "133****206",
+        "from": "LTSSB3",
+        "time": "8"
+    },
+
+    {
+        "number": "048****666",
+        "from": "YDSSB3",
+        "time": "138"
+    },
+
+    {
+        "number": "180****753",
+        "from": "LTSSB4",
+        "time": "32"
+    },
+
+    {
+        "number": "060****523",
+        "from": "GWC2SS",
+        "time": "17"
+    },
+
+    {
+        "number": "153****132",
+        "from": "LTSSB3",
+        "time": "8"
+    },
+
+    {
+        "number": "008****258",
+        "from": "XWTGW2",
+        "time": "0"
+    },
+
+    {
+        "number": "153****920",
+        "from": "LTSSB3",
+        "time": "20"
+    },
+
+    {
+        "number": "173****344",
+        "from": "LTSSB3",
+        "time": "16"
+    },
+
+    {
+        "number": "173****126",
+        "from": "LTSSB4",
+        "time": "11"
+    },
+
+    {
+        "number": "000****853",
+        "from": "民生SS1",
+        "time": "0"
+    },
+
+    {
+        "number": "046****728",
+        "from": "XWTGW2",
+        "time": "0"
+    },
+
+    {
+        "number": "072****986",
+        "from": "GWB1",
+        "time": "8"
+    },
+
+    {
+        "number": "060****777",
+        "from": "横浜SS2",
+        "time": "293"
+    },
+
+    {
+        "number": "072****186",
+        "from": "XWTGW2",
+        "time": "240"
+    },
+
+    {
+        "number": "060****720",
+        "from": "GWC1SS",
+        "time": "101"
+    },
+
+    {
+        "number": "060****183",
+        "from": "GWC1SS",
+        "time": "0"
+    },
+
+    {
+        "number": "180****680",
+        "from": "TSSB4",
+        "time": "0"
+    },
+
+    {
+        "number": "189****057",
+        "from": "LTSSB3",
+        "time": "54"
+    },
+
+    {
+        "number": "181****872",
+        "from": "LTSSB3",
+        "time": "16"
+    },
+
+    {
+        "number": "153****761",
+        "from": "LTSSB3",
+        "time": "9"
+    },
+
+    {
+        "number": "173****778",
+        "from": "LTSSB4",
+        "time": "0"
+    },
+
+    {
+        "number": "173****734",
+        "from": "LTSSB4",
+        "time": "20"
+    },
+
+    {
+        "number": "153****580",
+        "from": "LTSSB4",
+        "time": "18"
+    },
+
+    {
+        "number": "048****333",
+        "from": "XWTGW2",
+        "time": "63"
+    },
+
+    {
+        "number": "180****287",
+        "from": "LTSSB4",
+        "time": "20"
+    },
+
+    {
+        "number": "060****766",
+        "from": "横浜SS2",
+        "time": "69"
+    },
+
+    {
+        "number": "009****300",
+        "from": "XWTGW2",
+        "time": "254"
+    },
+
+    {
+        "number": "189****009",
+        "from": "LTSSB4",
+        "time": "18"
+    },
+
+    {
+        "number": "095****889",
+        "from": "TGW1",
+        "time": "290"
+    },
+
+    {
+        "number": "072****236",
+        "from": "GWA2",
+        "time": "14"
+    },
+
+    {
+        "number": "173****084",
+        "from": "LTSSB3",
+        "time": "21"
+    },
+
+    {
+        "number": "072****321",
+        "from": "GWA1",
+        "time": "9"
+    },
+
+    {
+        "number": "072****280",
+        "from": "GWA2",
+        "time": "13"
+    },
+
+    {
+        "number": "060****985",
+        "from": "2SS1",
+        "time": "9"
+    },
+
+    {
+        "number": "153****023",
+        "from": "LTSSB4",
+        "time": "14"
+    },
+
+    {
+        "number": "072****777",
+        "from": "GWA1",
+        "time": "10"
+    },
+
+    {
+        "number": "072****189",
+        "from": "GWB2",
+        "time": "15"
+    },
+
+    {
+        "number": "133****583",
+        "from": "LTSSB3",
+        "time": "8"
+    },
+
+    {
+        "number": "008****680",
+        "from": "LTSSB3",
+        "time": "109"
+    },
+
+    {
+        "number": "133****086",
+        "from": "LTSSB4",
+        "time": "88"
+    },
+
+    {
+        "number": "133****877",
+        "from": "LTSSB4",
+        "time": "63"
+    },
+
+    {
+        "number": "189****888",
+        "from": "LTSSB3",
+        "time": "140"
+    },
+
+    {
+        "number": "189****092",
+        "from": "LTSSB3",
+        "time": "82"
+    },
+
+    {
+        "number": "133****086",
+        "from": "LTSSB4",
+        "time": "28"
+    },
+
+    {
+        "number": "095****777",
+        "from": "XWTGW2",
+        "time": "16"
+    },
+
+    {
+        "number": "173****427",
+        "from": "LTSSB3",
+        "time": "14"
+    },
+
+    {
+        "number": "060****176",
+        "from": "GWC1SS",
+        "time": "24"
+    },
+
+    {
+        "number": "060****762",
+        "from": "横浜SS2",
+        "time": "198"
+    },
+
+    {
+        "number": "177****715",
+        "from": "LTSSB4",
+        "time": "19"
+    },
+
+    {
+        "number": "095****666",
+        "from": "4SS1",
+        "time": "333"
+    },
+
+    {
+        "number": "133****263",
+        "from": "LTSSB4",
+        "time": "29"
+    },
+
+    {
+        "number": "061****666",
+        "from": "GWC1SS",
+        "time": "8"
+    },
+
+    {
+        "number": "060****183",
+        "from": "GWC1SS",
+        "time": "0"
+    },
+
+    {
+        "number": "072****106",
+        "from": "民生SS1",
+        "time": "12"
+    },
+
+    {
+        "number": "173****706",
+        "from": "LTSSB4",
+        "time": "17"
+    },
+
+    {
+        "number": "095****888",
+        "from": "XWTGW1",
+        "time": "118"
+    },
+
+    {
+        "number": "153****089",
+        "from": "LTSSB4",
+        "time": "38"
+    },
+
+    {
+        "number": "060****761",
+        "from": "横浜SS2",
+        "time": "122"
+    },
+
+    {
+        "number": "133****300",
+        "from": "LTSSB4",
+        "time": "1"
+    },
+
+    {
+        "number": "060****392",
+        "from": "GWC1SS",
+        "time": "25"
+    },
+
+    {
+        "number": "153****920",
+        "from": "LTSSB4",
+        "time": "76"
+    },
+
+    {
+        "number": "173****026",
+        "from": "LTSSB4",
+        "time": "7"
+    },
+
+    {
+        "number": "072****689",
+        "from": "GWA1",
+        "time": "9"
+    },
+
+    {
+        "number": "048****996",
+        "from": "4SS2",
+        "time": "21"
+    }
+
+]
+
+
